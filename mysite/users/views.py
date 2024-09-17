dc10da8b08b87f0685c6e2a79fd5648039a2c3b4
SouaOui <soumiaouzat@gmail.com>
2024-09-17 18:44:01 +0100

    Register Page & Adding Users to DataBase

---
+++ mysite/users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Congrats{username}! Now You Can Log in')
+            return redirect('home-page')
+    else:
+        form = UserRegisterForm()
+    return render(request, "register.html", {'form': form})
+    
