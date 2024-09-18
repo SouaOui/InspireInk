0ce50417789e38d05bcdd4c94da5d872fe2f0f68
SouaOui <your.email@example.com>
2024-09-18 22:48:06 +0100

    adding user profile & custom nav bar depending on user's state

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -8,3 +8,14 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        help_texts = {
+            'username': 'Choose a unique username',
+            'email': 'Enter a valid email address.',
+            'password1': 'password with at least 8 characters.',
+            'password2': 'Re-enter your password to confirm.',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].help_text = 'password with at least 8 characters.'
+        self.fields['password2'].help_text = 'Re-enter your password to confirm.'
